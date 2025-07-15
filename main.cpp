c8ae41f4f2ba7011ec8762f3edf727e5b4d00fcc
jac <jacllovey@qq.com>
2025-07-15 20:39:03 +0800

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -3,6 +3,6 @@
        svbool_t pg = svptrue_b16();
        svuint16_t u16_a, u16_b;
        svuint32_t u32_c = svdup_u32(0);
-       u32_c = svmlalt_u32(pg, u32_c, u16_a, u16_b);
+       u32_c = svmlalt_u32(u32_c, u16_a, u16_b);
        return 0;
    }
